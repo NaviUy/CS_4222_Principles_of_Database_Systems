22764b2115289c9cbf609f1a4e3fca80afe9db3a
Ivan Yu <ivanscreativity@gmail.com>
2019-09-25 22:00:00 -0700

    added assignment 2

---
+++ Assignment 2/Assignment2.sql
@@ -0,0 +1,100 @@
+-- 1. List the department managers who are controlling more than 2 projects.
+
+SELECT Fname, Lname 
+FROM EMPLOYEE 
+WHERE Ssn 
+	IN (SELECT Mgr_ssn 
+		FROM DEPARTMENT 
+		WHERE Dnumber 
+			IN (SELECT Dnum 
+				FROM PROJECT 
+				GROUP BY Dnum 
+				HAVING COUNT(Dnum) > 2));
+ 
+-- 2. Retrieve the names of the departments that only located in ‘Houston’.
+
+SELECT Dname
+FROM DEPARTMENT
+WHERE Dnumber
+	IN (SELECT Dnumber 
+    	FROM DEPT_LOCATIONS
+    	WHERE Dlocation = "Houston")
+AND
+NOT Dname =
+  		(SELECT Dname 
+  		FROM DEPARTMENT 
+   		WHERE Dnumber
+  		IN (SELECT Dnumber 
+  			FROM DEPT_LOCATIONS 
+  			WHERE Dnumber 
+  			GROUP BY Dnumber 
+  			HAVING COUNT(Dnumber) > 1));	
+			
+-- 3. List the names of employees all of whose dependents were born before 1960.
+ 
+SELECT Fname, Lname
+FROM EMPLOYEE
+WHERE Ssn 
+	IN(SELECT Essn 
+	FROM DEPENDENT 
+	WHERE YEAR(Bdate) < 1960);
+  		
+
+-- 4. List the names of departments managed by the direct subordinate of the manager of ‘Headquarters’ department.
+
+SELECT Dname
+FROM DEPARTMENT
+WHERE Mgr_ssn 
+	IN(SELECT Ssn
+	FROM EMPLOYEE
+	WHERE Ssn
+		IN(SELECT Ssn
+		FROM EMPLOYEE 
+		WHERE Super_ssn
+			In(SELECT Mgr_ssn
+			FROM DEPARTMENT 
+			WHERE Dname = "Headquarters")));
+	
+-- 5. Retrieve the average salary of male employees who work totally no less than 50 hours on projects.
+
+SELECT AVG(Salary)
+FROM EMPLOYEE
+WHERE Ssn 
+	IN(SELECT Essn
+	FROM WORKS_ON
+	GROUP BY Essn
+	HAVING SUM(Hours) >= 50);	
+
+-- 6. Find the names of projects that all direct subordinates of James Borg work for.
+
+SELECT Pname
+FROM PROJECT
+WHERE Dnum
+	 IN (SELECT Dno
+	 FROM EMPLOYEE
+	 WHERE Super_ssn
+	 	IN(SELECT Ssn
+	 	FROM EMPLOYEE
+	 	WHERE Fname = "James" AND Lname = "Borg"));
+ 
+-- 7. Show the name of employee who and whose supervisor are in different departments.
+
+SELECT emp.Fname, emp.Super_ssn, emp.Dno
+FROM EMPLOYEE as emp
+LEFT JOIN EMPLOYEE as sup
+ON emp.Super_ssn = sup.Ssn 
+WHERE emp.Dno != sup.Dno;
+
+-- 8. Find the name of employee who only has spouse as the dependent.
+
+SELECT Fname, Lname 
+FROM EMPLOYEE as emp
+WHERE emp.Ssn 
+	IN (SELECT dep.Essn 
+	FROM DEPENDENT as dep
+	WHERE dep.Relationship = 'Spouse') 
+	AND emp.Ssn 
+		IN (SELECT dep2.Essn 
+		FROM DEPENDENT as dep2
+		GROUP BY dep2.Essn 
+		HAVING Count(dep2.Essn) = 1);
